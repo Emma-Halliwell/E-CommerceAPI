63bbd1cae3347fc02c2383bdc25c3116a58d2e3c
Emzy <132276370+Emma-Halliwell@users.noreply.github.com>
2024-06-03 18:03:39 +0100

    Header component started.

--- frontend/e-commerce-api-part2/src/components/Header.js
+++ frontend/e-commerce-api-part2/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import companyLogo from './SportsGaloreLogo.jpeg';
+//import { useDispatch, useSelector } from 'react-redux';
 
 import { NavLink } from 'react-router-dom';
 
 export default function Header () {
     return (
         <div className='header'>
+            <img src={companyLogo} alt="Sports Galore Logo" width="100px" height="100px"/>
             <NavLink to="/">Home</NavLink>
         </div>
     )
