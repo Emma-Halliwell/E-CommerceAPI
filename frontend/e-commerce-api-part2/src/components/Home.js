88e3738042c0b8c63f61cd8cddc99d6263768cd7
Emzy <132276370+Emma-Halliwell@users.noreply.github.com>
2024-09-24 16:09:07 +0100

    minor changes

--- frontend/e-commerce-api-part2/src/components/Home.js
+++ frontend/e-commerce-api-part2/src/components/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
 import Search from '../features/Search';
 import ItemContainer from '../features/ItemContainer';
 
